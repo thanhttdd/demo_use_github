e3885add6e2634b6f2f4b94f9a5f7b1e2b261db8
thanhttdd <thanh.tt@digidinos.com>
2021-06-09 17:09:14 +0700

    example repository

---
+++ RepositoryUsingEFinMVC/RepositoryUsingEFinMVC/GenericRepository/GenericRepository.cs
@@ -0,0 +1,58 @@
+﻿using RepositoryUsingEFinMVC.DAL;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace RepositoryUsingEFinMVC.GenericRepository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private EmployeeDBContext _context = null;
+        private DbSet<T> table = null;
+
+        public GenericRepository()
+        {
+            this._context = new EmployeeDBContext();
+            table = _context.Set<T>();
+        }
+
+        public GenericRepository(EmployeeDBContext _context)
+        {
+            this._context = _context;
+            table = _context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return table.ToList();
+        }
+
+        public T GetById(object id)
+        {
+            return table.Find(id);
+        }
+
+        public void Insert(T obj)
+        {
+            table.Add(obj);
+        }
+
+        public void Update(T obj)
+        {
+            table.Attach(obj);
+            _context.Entry(obj).State = EntityState.Modified;
+        }
+
+        public void Delete(object id)
+        {
+            T existing = table.Find(id);
+            table.Remove(existing);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
+//https://dotnettutorials.net/lesson/generic-repository-pattern-csharp-mvc/
---
+++ RepositoryUsingEFinMVC/RepositoryUsingEFinMVC/Repository/IEmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using RepositoryUsingEFinMVC.DAL;
+using System.Collections.Generic;
+namespace RepositoryUsingEFinMVC.Repository
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+        Employee GetById(int EmployeeID);
+        void Insert(Employee employee);
+        void Update(Employee employee);
+        void Delete(int EmployeeID);
+        void Save();
+    }
+}
