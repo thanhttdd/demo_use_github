0bb0d601e13c57d23ff6901b47a07228605bf6e4
thanhttdd <thanh.tt@digidinos.com>
2021-06-14 15:45:29 +0700

    Add unit of work to project

---
+++ RepositoryUsingEFinMVC/RepositoryUsingEFinMVC/Controllers/Employee01Controller.cs
@@ -0,0 +1,26 @@
+﻿using RepositoryUsingEFinMVC.DAL;
+using RepositoryUsingEFinMVC.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RepositoryUsingEFinMVC.Controllers
+{
+    public class Employee01Controller : Controller
+    {
+        private readonly UnitOfWorkDemo _unitOfWork;
+
+        public Employee01Controller()
+        {
+            this._unitOfWork = new UnitOfWorkDemo(new EmployeeDBContext());
+        }
+
+        // GET: Employee01
+        public ActionResult Index()
+        {
+            return View(this._unitOfWork.EmployeeRepository.GetAll());
+        }
+    }
+}
---
+++ RepositoryUsingEFinMVC/RepositoryUsingEFinMVC/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using RepositoryUsingEFinMVC.DAL;
+using RepositoryUsingEFinMVC.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RepositoryUsingEFinMVC.UnitOfWork
+{
+    public class UnitOfWorkDemo
+    {
+        private IEmployeeRepository _employeeRepository;
+        private EmployeeDBContext _dbContext;
+
+        public UnitOfWorkDemo(EmployeeDBContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public IEmployeeRepository EmployeeRepository
+        {
+            get
+            {
+                if (_employeeRepository == null)
+                    _employeeRepository = new EmployeeRepository();
+                return _employeeRepository;
+            }
+        }
+
+        public void Save()
+        {
+            this._dbContext.SaveChanges();
+        }
+    }
+}
